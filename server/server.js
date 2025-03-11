c9668ef7e1c25114986844bba0d6af80f1ff536d
DerEns86 <v.ens86@yahoo.de>
2025-03-11 16:11:46 +0100

    Add Socket.IO to backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const io = require("socket.io")(http, {
+  cors: {
+    origin: ["http://localhost:5137"],
+  },
+});
 
 const app = express();
 app.use(cors());
@@ -7,6 +13,13 @@ app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
