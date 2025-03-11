317be6322db7c2b40b511c101cffa78bf311b579
DerEns86 <v.ens86@yahoo.de>
2025-03-11 15:12:19 +0100

    first commit

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+fetch('http://localhost:5000/api/hello')
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error('Error:', error))
+</script>
+
+<template>
+  <main></main>
+</template>
